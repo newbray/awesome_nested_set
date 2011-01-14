4f20b11c938b4b34fc487287600f3e7f17fba58c
newbray <chris_guy_84@hotmail.com>
2011-01-14 08:50:58 -0800

    added nested_set_association_options_for_select helper

--- lib/awesome_nested_set/helper.rb
+++ lib/awesome_nested_set/helper.rb
@@ -34,6 +34,52 @@ module CollectiveIdea #:nodoc:
           end
           result
         end
+
+       # This method helps with building options_for_select for an object that has an association to a nested_set model
+       # returns options_for_select with the following rules:
+       # - class_or_item must be a class (or object of a class) that uses acts_as_nested_set
+       # - items listed in excluded_items array will not be included in the list (to be used for items already associated to the object the drop down is for)
+       # - all leaf nodes (ie items without sub-items) not included in the excluded_items array will appear in the drop-down and be selectable
+       # - items that have children will be added to the list but will be disabled
+       # - items with children who have ALL their children included in the excluded_items array will not be shown (as there are no selectable children)
+  
+       # example usage: <%= select :category, :id, nested_set_association_options_for_select(Item, @item.categories, "Select a category...") %>
+  
+       def nested_set_association_options_for_select(class_or_item, excluded_items=[], prompt="Please select..." )
+    
+         throw ArgumentError.new "nested_set_association_options_for_select - class_or_item must use acts_as_nested_set" unless class_or_item.respond_to?(:roots)
+    
+         def get_children(node, excluded_items = [] )
+           result = []
+           collection = nil
+           if node.is_a?(Class)
+             collection = Array(node.roots)
+           else
+             collection = Array(node.children)
+           end
+           collection.each do |child|
+             if child.children.empty?
+               result << ["#{ '-' * child.level} #{child.to_label}", child.id.to_s] unless excluded_items.include?(child)
+             else
+               children = get_children(child, excluded_items)
+               unless children.empty?
+                 @disabled << child.id.to_s
+                 result << ["#{ '-' * child.level} #{child.to_label}", child.id.to_s]
+                 result += children
+               end
+             end
+           end
+           result
+         end
+    
+         result = [[prompt, ""]]
+         @disabled = []
+    
+         result += get_children(class_or_item, excluded_items)    
+    
+         options_for_select(result, {:disabled => @disabled })
+       end
+
       end
     end
   end
